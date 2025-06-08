5c64d17e9a8736f860930e5b33cbbed217757bdb
Yury Senna <yurysenasilva@gmail.com>
2025-06-07 21:52:06 -0300

    testando

---
+++ script.js
@@ -0,0 +1,25 @@
+const key = "a578fa374c35314a5e3ef055159a72f4"
+
+function colocarDadosNaTela(dados) {
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+    console.log(dados)
+
+}
+
+async function buscarCidade(cidade) {
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
+
+    colocarDadosNaTela(dados)
+
+}
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}    
